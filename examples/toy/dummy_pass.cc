55a74841a76f3040d78fdac8e96ce6e5cfc95e7d
Sungsoon Cho <scho@anaflash.com>
2023-03-24 06:03:14 +0900

    Replace TypeRange with SmallVector from the pass, too.

--- examples/toy/dummy_pass.cc
+++ examples/toy/dummy_pass.cc
@@ -17,15 +17,17 @@ class DummyPass : public PassWrapper<DummyPass, OperationPass<ModuleOp>> {
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(DummyPass)
 
   void runOnOperation() override {
+    using TypeVector = llvm::SmallVector<Type, 4>;
+
     OpBuilder builder(&getContext());
     llvm::SmallVector<long int, 4> tensor_shape = {32, 64, 2};
     // Toy dialect requires the element type to be F64.
     Type element_type = builder.getF64Type();
     RankedTensorType tensor_type =
         RankedTensorType::get(tensor_shape, element_type);
-    TypeRange input_types = {tensor_type};
+    TypeVector input_types = {tensor_type};
     // Toy dialect requires the number of return values to be 1.
-    TypeRange output_types = {tensor_type};
+    TypeVector output_types = {tensor_type};
     FunctionType func_type =
         FunctionType::get(&getContext(), input_types, output_types);
     Location unknown_loc = builder.getUnknownLoc();
