4c73b0be3f16a8625a0247bbfc17af7cabda58d5
Sungsoon Cho <sungsoon.cho@gmail.com>
2023-11-30 19:09:35 -0800

    Start conv1d example.

---
+++ examples/conv/conv1d.cc
@@ -0,0 +1,16 @@
+#include "examples/conv/conv1d.h"
+
+namespace sungcho {
+
+int get_output_size(int input_size, int kernel_size, int stride) {
+  // TODO(scho):
+  return -1;
+}
+
+std::vector<float> conv1d(const std::vector<float>& image,
+                          const std::vector<float>& filter, int stride) {
+  // TODO(scho):
+  return {};
+}
+
+}  // namespace sungcho
---
+++ examples/conv/conv1d_test.cc
@@ -0,0 +1,12 @@
+#include "examples/conv/conv1d.h"
+
+#include "absl/log/log.h"
+#include "gtest/gtest.h"
+
+namespace sungcho {
+namespace {
+
+TEST(Conv1DTest, Basic) { LOG(INFO) << "xxxxx"; }
+
+}  // namespace
+}  // namespace sungcho
